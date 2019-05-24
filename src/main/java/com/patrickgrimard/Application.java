daea1b4327101e9e37050ad529e46cccc78c786d
jprakash197 <50922701+jprakash197@users.noreply.github.com>
2019-05-25 00:21:10 +0530

    Update Application.java

--- src/main/java/com/patrickgrimard/Application.java
+++ src/main/java/com/patrickgrimard/Application.java
@@ -16,4 +16,8 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+ //    public static void main(String[] args) {
+ //       SpringApplication.run(Application.class, args);
+ //   }
+    
 }
